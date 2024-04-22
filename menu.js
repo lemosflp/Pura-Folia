f9b5191b83cf0b18871aff20bfe81078f2fa7258
Felipe Lemos <felipelemoso500@gmail.com>
2024-04-22 11:01:09 -0300

    new background wallpaper

--- menu.js
+++ menu.js
@@ -1,18 +1,18 @@
-window.addEventListener("scroll", function(){
-    let header = document.querySelector('#header')
-    header.classList.toggle('rolagem',window.scrollY > 250)
-})
+    window.addEventListener("scroll", function(){
+        let header = document.querySelector('#header')
+        header.classList.toggle('rolagem',window.scrollY > 250)
+    })
 
-window.addEventListener('scroll', function() {
-    var botaoVoltar = document.getElementById('botao-voltar');
-    if (window.scrollY > 50 * window.innerHeight / 50) {
-        botaoVoltar.style.display = 'block'; // Mostra o botão
-    } else {
-        botaoVoltar.style.display = 'none'; // Oculta o botão
+    window.addEventListener('scroll', function() {
+        var botaoVoltar = document.getElementById('botao-voltar');
+        if (window.scrollY > 50 * window.innerHeight / 50) {
+            botaoVoltar.style.display = 'block'; // Mostra o botão
+        } else {
+            botaoVoltar.style.display = 'none'; // Oculta o botão
+        }
+    });
+
+    function voltarAoInicio() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     }
-});
-
-function voltarAoInicio() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
 
